1970975b897b4f56d13f5515ffa9b8685a19fee8
anjanpl <anjanpl@yahoo.com>
2017-08-10 23:32:12 +0600

    Add files via upload

---
+++ VisitelOigUtility/OigFederalExclusions.vb
@@ -0,0 +1,240 @@
+﻿Imports System.Data
+Imports System.Data.OleDb
+Imports System.Data.SqlClient
+Imports System.Net
+Imports System.IO
+Imports System.Collections.Specialized
+Imports VisitelDA.VisitelDA
+Imports ICSharpCode.SharpZipLib.Zip
+
+Namespace VisitelOigUtility
+    Public Class OigFederalExclusions
+
+        Private tableName As String = "[TurboDB.OigFederalExclusions]"
+
+        ''' <summary>
+        ''' Downloads and saves OIG Federal Exclusions Database and saves on specified location
+        ''' </summary>
+        ''' <param name="downloadUrl">OIG Federal Exclusions Database url</param>
+        ''' <param name="downloadDirectory">Directory path of downloaded file</param>
+        ''' <param name="fileName">Downloaded file name</param>
+        ''' <returns>Error Message</returns>
+        ''' <remarks></remarks>
+        Public Function DownloadOigFile(downloadUrl As String, downloadDirectory As String, fileName As String) As String
+            Dim errorMessage As String = String.Empty
+
+            Try
+                If (Not Directory.Exists(downloadDirectory)) Then
+                    Directory.CreateDirectory(downloadDirectory)
+                End If
+                Dim webClient As New WebClient()
+                webClient.DownloadFile(downloadUrl, String.Format("{0}{1}", downloadDirectory, fileName))
+            Catch ex As Exception
+                errorMessage = "Can not download file"
+            End Try
+
+            Return errorMessage
+        End Function
+
+        ''' <summary>
+        ''' Unzips downloaded .zip file
+        ''' </summary>
+        ''' <param name="zipFileDirectory">Zip file directory</param>
+        ''' <param name="zipFileName">Zip file name</param>
+        ''' <returns>Error Message</returns>
+        ''' <remarks></remarks>
+        Public Function UnzipOigFile(zipFileDirectory As String, zipFileName As String) As String
+            Dim errorMessage As String = String.Empty
+
+            Try
+                Using zipInputStream As ZipInputStream = New ZipInputStream(File.OpenRead(String.Format("{0}{1}", zipFileDirectory, zipFileName)))
+                    Dim objEntry As ZipEntry
+
+                    objEntry = zipInputStream.GetNextEntry()
+                    While IsNothing(objEntry) = False
+
+                        Dim directoryName As String = Path.GetDirectoryName(objEntry.Name)
+                        Dim fileName As String = Path.GetFileName(objEntry.Name)
+
+                        'create directory
+                        If (directoryName.Length > 0) Then
+                            Directory.CreateDirectory(directoryName)
+                        End If
+
+                        If (Not String.IsNullOrEmpty(fileName)) Then
+                            Using streamWriter As FileStream = File.Create(String.Format("{0}{1}", zipFileDirectory, objEntry.Name))
+                                Dim size As Integer = 2048
+                                Dim data(size) As Byte
+
+                                While True
+                                    size = zipInputStream.Read(data, 0, data.Length)
+                                    If (size > 0) Then
+                                        streamWriter.Write(data, 0, size)
+                                    Else
+                                        Exit While
+                                    End If
+                                End While
+                            End Using
+                        End If
+
+                        objEntry = zipInputStream.GetNextEntry()
+
+                    End While
+                End Using
+
+                File.Delete(String.Format("{0}{1}", zipFileDirectory, zipFileName))
+            Catch ex As Exception
+                errorMessage = "Can not unzip file"
+            End Try
+
+            Return errorMessage
+        End Function
+
+        ''' <summary>
+        ''' Reads Federal Execlusions Database (.dbf) file and updates Federal OIG table
+        ''' </summary>
+        ''' <param name="fileDirectory">File (.dbf) Directory</param>
+        ''' <param name="fileNameWithoutExtension">File (.dbf) name without extension</param>
+        ''' <param name="ConVisitel">SqlConnection</param>
+        ''' <returns>Error Message</returns>
+        ''' <remarks></remarks>
+        Public Function ReadAndUpdateOigExclusionsDatabase(fileDirectory As String, fileNameWithoutExtension As String, ConVisitel As SqlConnection) As String
+            Dim errorMessage As String = String.Empty
+
+            Try
+                Dim dataTable As DataTable
+                errorMessage = "Can not read Federal Exclusions Database"
+
+                If (Environment.Is64BitProcess) Then
+                    ''Requires Microsoft Access Database Engine 2010
+                    ''errorMessage = errorMessage + " using 64Bit"
+                    dataTable = ReadDbfUsingAceOledb(fileDirectory, fileNameWithoutExtension)
+                Else
+                    dataTable = ReadDbfUsingJetOledb(fileDirectory, fileNameWithoutExtension)
+                    ''dataTable = ReadDbfUsingOdbc(fileDirectory, fileNameWithoutExtension)
+                End If
+
+                errorMessage = "Can not update Federal OIG table"
+                WriteToDatabase(ConVisitel, dataTable)
+                errorMessage = String.Empty
+            Catch ex As Exception
+                errorMessage = errorMessage + " because: " + ex.Message
+            End Try
+
+            Return errorMessage
+        End Function
+
+        Private Function ReadDbfUsingJetOledb(fileDirectory As String, fileNameWithoutExtension As String) As DataTable
+            Dim dataTable As DataTable = Nothing
+
+            Try
+                Dim connectionString As String = String.Format("Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties=dBASE IV;User ID=Admin;Password=;", fileDirectory)
+                Dim selectQuery = String.Format("SELECT * FROM {0}", fileNameWithoutExtension)
+
+                Using oConn As OleDbConnection = New OleDbConnection(connectionString)
+                    dataTable = New DataTable("UpdatedTable")
+                    Dim dataAdapter As OleDbDataAdapter = New OleDbDataAdapter(selectQuery, oConn)
+                    dataAdapter.Fill(dataTable)
+                    dataAdapter.Dispose()
+                End Using
+            Catch ex As Exception
+                Throw ex
+            End Try
+
+            Return dataTable
+        End Function
+
+        Private Function ReadDbfUsingOdbc(fileDirectory As String, fileNameWithoutExtension As String) As DataTable
+            Dim dataTable As DataTable = Nothing
+
+            Try
+                Dim connectionString As String = "Driver={Microsoft dBASE Driver (*.dbf)};Driverid=277;" + String.Format("Dbq={0}", fileDirectory)
+                Dim selectQuery = String.Format("SELECT * FROM {0}", fileNameWithoutExtension)
+
+                Using oConn As OleDbConnection = New OleDbConnection(connectionString)
+                    dataTable = New DataTable("UpdatedTable")
+                    Dim dataAdapter As OleDbDataAdapter = New OleDbDataAdapter(selectQuery, oConn)
+                    dataAdapter.Fill(dataTable)
+                End Using
+            Catch ex As Exception
+                Throw ex
+            End Try
+
+            Return dataTable
+        End Function
+
+        Private Function ReadDbfUsingAceOledb(fileDirectory As String, fileNameWithoutExtension As String) As DataTable
+            Dim dataTable As DataTable = Nothing
+
+            Try
+                Dim connectionString As String = String.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=dBASE IV;User ID=Admin;", fileDirectory)
+                Dim selectQuery = String.Format("SELECT * FROM {0}", fileNameWithoutExtension)
+
+                Using oConn As OleDbConnection = New OleDbConnection(connectionString)
+                    dataTable = New DataTable("UpdatedTable")
+                    Dim dataAdapter As OleDbDataAdapter = New OleDbDataAdapter(selectQuery, oConn)
+                    dataAdapter.Fill(dataTable)
+                End Using
+            Catch ex As Exception
+                Throw ex
+            End Try
+
+            Return dataTable
+        End Function
+
+        Private Function WriteToDatabase(ConVisitel As SqlConnection, dataTable As DataTable) As String
+            Dim errorMessage As String = String.Empty
+
+            Try
+                Dim objSharedSettings As New SharedSettings()
+
+                objSharedSettings.ExecuteQuery(String.Format("TRUNCATE TABLE {0}", tableName), ConVisitel)
+
+                Dim bulkCopy As SqlBulkCopy = New SqlBulkCopy(ConVisitel)
+                bulkCopy.DestinationTableName = tableName
+                bulkCopy.WriteToServer(dataTable)
+
+                Dim parameters As New HybridDictionary()
+                parameters.Add("@TableName", tableName)
+                parameters.Add("@UpdateBy", "")
+                objSharedSettings.ExecuteQuery("", "[TurboDB.InsertTableUpdateLog]", ConVisitel, parameters)
+
+                objSharedSettings = Nothing
+                parameters = Nothing
+            Catch ex As Exception
+                Throw ex
+            End Try
+
+            Return errorMessage
+        End Function
+
+        Public Function GetLastUpdatedDateTime(ConVisitel As SqlConnection) As DateTime
+            Dim returnValue As DateTime = DateTime.MinValue
+
+            Try
+                Dim objSharedSettings As New SharedSettings()
+                Dim drSql As SqlDataReader = Nothing
+                Dim parameters As New HybridDictionary()
+
+                parameters.Add("@TableName", tableName)
+                objSharedSettings.GetDataReader("", "[TurboDB.SelectTableUpdateLog]", drSql, ConVisitel, parameters)
+
+                objSharedSettings = Nothing
+                parameters = Nothing
+
+                If drSql.HasRows Then
+                    While drSql.Read()
+                        returnValue = If((DBNull.Value.Equals(drSql("UpdateDate"))),
+                                              DateTime.MinValue, Convert.ToDateTime(drSql("UpdateDate"), Nothing))
+                    End While
+                End If
+
+            Catch ex As Exception
+                Throw ex
+            End Try
+
+            Return returnValue
+        End Function
+
+    End Class
+End Namespace
---
+++ VisitelOigUtility/OigStateExclusions.vb
@@ -0,0 +1,162 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Collections.Specialized
+Imports VisitelDA.VisitelDA
+Imports System.IO
+
+Namespace VisitelOigUtility
+    Public Class OigStateExclusions
+        Private tableName As String = "[TurboDB.OigSateExclusions]"
+
+        Public Function ReadAndUpdateOigExclusionsDatabase(filePath As String, ConVisitel As SqlConnection) As String
+            Dim errorMessage As String = String.Empty
+
+            Try
+                If (File.Exists(filePath)) Then
+                    Dim dataTable As DataTable
+                    errorMessage = "Can not read State Exclusions Database"
+
+                    dataTable = ReadTextFile(filePath)
+
+                    errorMessage = "Can not update State OIG table"
+                    WriteToDatabase(ConVisitel, dataTable)
+                    errorMessage = String.Empty
+                Else
+                    errorMessage = "File not found"
+                End If
+
+            Catch ex As Exception
+            End Try
+
+            Return errorMessage
+        End Function
+
+        Private Function ReadTextFile(filePath As String) As DataTable
+            Dim dataTable As DataTable = Nothing
+
+            Dim file As System.IO.StreamWriter
+            file = My.Computer.FileSystem.OpenTextFileWriter("D:\task\Johnny Nosike\Bitbucket\Visitel\VisitelWeb\OIG\test.txt", True)
+
+            Dim file__1 As String = System.IO.File.ReadAllText(filePath).Replace(vbCr & vbLf, " ")
+
+            Dim file1 As New FileStream(filePath, FileMode.Open, FileAccess.ReadWrite)
+
+            Dim wr As New StreamWriter(file1)
+
+            ' Write your content here
+            wr.Write(file__1)
+
+            wr.Close()
+            file1.Close()
+
+
+            Try
+                Dim line As String
+                Dim lineNo As Integer = 0
+
+                Using reader As StreamReader = New StreamReader(filePath)
+                    line = reader.ReadLine()
+                    lineNo = lineNo + 1
+
+                    While (Not line Is Nothing)
+                        Dim columnValues() As String = line.Split(New String() {vbTab}, StringSplitOptions.None)
+                        Dim dataRow As DataRow = Nothing
+                        Dim columnIndex As Integer = 0
+
+                        For Each value As String In columnValues
+                            If (lineNo = 1) Then
+                                If (dataTable Is Nothing) Then
+                                    dataTable = New DataTable("UpdatedTable")
+                                End If
+
+                                dataTable.Columns.Add(value.Replace("""", ""))
+                            Else
+                                If (dataRow Is Nothing) Then
+                                    dataRow = dataTable.NewRow()
+                                    dataTable.Rows.Add(dataRow)
+                                End If
+
+                                dataRow(columnIndex) = value.Replace("""", "")
+                            End If
+
+                            columnIndex = columnIndex + 1
+                        Next
+
+                        Try
+                            line = reader.ReadLine()
+                            file.WriteLine(line)
+                        Catch ex As Exception
+
+                        End Try
+
+
+
+
+
+                        lineNo = lineNo + 1
+                    End While
+                End Using
+            Catch ex As Exception
+                File.Close()
+                Throw ex
+            End Try
+
+            Return dataTable
+        End Function
+
+        Private Function WriteToDatabase(ConVisitel As SqlConnection, dataTable As DataTable) As String
+            Dim errorMessage As String = String.Empty
+
+            Try
+                Dim objSharedSettings As New SharedSettings()
+
+                objSharedSettings.ExecuteQuery(String.Format("TRUNCATE TABLE {0}", tableName), ConVisitel)
+
+                Dim bulkCopy As SqlBulkCopy = New SqlBulkCopy(ConVisitel)
+                bulkCopy.DestinationTableName = tableName
+                bulkCopy.WriteToServer(dataTable)
+
+                Dim parameters As New HybridDictionary()
+                parameters.Add("@TableName", tableName)
+                parameters.Add("@UpdateBy", "")
+                objSharedSettings.ExecuteQuery("", "[TurboDB.InsertTableUpdateLog]", ConVisitel, parameters)
+
+                objSharedSettings = Nothing
+                parameters = Nothing
+            Catch ex As Exception
+                Throw ex
+            End Try
+
+            Return errorMessage
+        End Function
+
+        Public Function GetLastUpdatedDateTime(ConVisitel As SqlConnection) As DateTime
+            Dim returnValue As DateTime = DateTime.MinValue
+
+            Try
+                Dim objSharedSettings As New SharedSettings()
+                Dim drSql As SqlDataReader = Nothing
+                Dim parameters As New HybridDictionary()
+
+                parameters.Add("@TableName", tableName)
+                objSharedSettings.GetDataReader("", "[TurboDB.SelectTableUpdateLog]", drSql, ConVisitel, parameters)
+
+                objSharedSettings = Nothing
+                parameters = Nothing
+
+                If drSql.HasRows Then
+                    While drSql.Read()
+                        returnValue = If((DBNull.Value.Equals(drSql("UpdateDate"))),
+                                              DateTime.MinValue, Convert.ToDateTime(drSql("UpdateDate"), Nothing))
+                    End While
+                End If
+
+            Catch ex As Exception
+                Throw ex
+            End Try
+
+            Return returnValue
+        End Function
+
+    End Class
+End Namespace
